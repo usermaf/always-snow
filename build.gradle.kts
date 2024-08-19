54e62fca947d47c21b153545b8d63a3cfa0e38da
b <134753907+usermaf@users.noreply.github.com>
2024-08-19 06:35:50 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,16 +14,16 @@ repositories {
 }
 
 dependencies {
-    minecraft("com.mojang:minecraft:1.20.4")
+    minecraft("com.mojang:minecraft:1.21")
     mappings(loom.officialMojangMappings())
-    modImplementation("net.fabricmc:fabric-loader:0.15.3")
-    modImplementation("net.fabricmc.fabric-api:fabric-api:0.91.3+1.20.4")
-    modImplementation("net.fabricmc:fabric-language-kotlin:1.10.17+kotlin.1.9.22")
-    modApi("me.shedaniel.cloth:cloth-config-fabric:13.0.121") {
+    modImplementation("net.fabricmc:fabric-loader:0.16.2")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.102.0+1.21")
+    modImplementation("net.fabricmc:fabric-language-kotlin:1.11.0+kotlin.2.0.0")
+    modApi("me.shedaniel.cloth:cloth-config-fabric:15.0.128") {
         exclude("net.fabricmc.fabric-api")
     }
-    modApi("com.terraformersmc:modmenu:9.0.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+    modApi("com.terraformersmc:modmenu:11.0.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
 }
 
 
